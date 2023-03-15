05048d515a384db03e7382c27eb8a07b0db5e18f
JoseEmmanuelVG <89156254+JoseEmmanuelVG@users.noreply.github.com>
2023-03-15 14:12:13 -0600

    Add files via upload

---
+++ Operaciones_AndOrXor_Imagenes_JEVG/Operations_JEVG.m
@@ -0,0 +1,87 @@
+%% Clean the window and data, Put a valor to variables Contrast and the Bias
+clc, clear
+close all;
+C=1;
+B=0.5;
+%% Read the image and Convert the to the same size
+% Load the two images you want to resize
+a = imread('Monedas.jpg');
+b = imread('Cuadros.jpg');
+% Determine the size of the larger image
+max_size = max(size(a), size(b));
+% Resize both images to the maximum size
+x = imresize(a, max_size(1:2));
+y = imresize(b, max_size(1:2));
+
+
+%% Gray  and invert the colors
+x2 = imcomplement(x);
+I = rgb2gray(x2);
+
+y2 = imcomplement(y);
+l = rgb2gray(y2);
+
+%% Pa to Binary and fill the holes
+xBinary = imbinarize(I);
+xBinaryPro = imfill(xBinary,'holes');
+
+yBinary = imbinarize(l);
+yBinaryPro = imfill(yBinary,'holes');
+
+%% invert the colors to the final Binary Image 
+xBinaryInvert = imcomplement(xBinaryPro);
+
+yBinaryInvert = imcomplement(yBinaryPro);
+
+
+%% Operations &, !!, XOR 
+and_img = bitand(xBinaryInvert, yBinaryInvert);
+or_img = bitor(xBinaryInvert, yBinaryInvert);
+xor_img = bitxor(xBinaryInvert, yBinaryInvert);
+
+and_img2 = bitand(xBinaryPro, yBinaryPro);
+or_img2 = bitor(xBinaryPro, yBinaryPro);
+xor_img2 = bitxor(xBinaryPro, yBinaryPro);
+
+
+%% Plot the images in the same figure
+Fig = figure('Name', 'T2. Identificación de monedas Binario  - JEVG');
+set(Fig, 'Position', [500 0 1250 1250])
+subplot(4,3,2)
+imshowpair(x,x2,'montage')
+title('Imagen Original e Imagen Invertida')
+subplot(4,3,1)
+imshow(xBinaryInvert)
+title('Imagen sin Huecos con Color Invertido')
+subplot(4,3,3)
+imshow(xBinaryPro)
+title('Imagen sin Huecos')
+    subplot(4,3,5)
+    imshowpair(y,y2,'montage')
+    title('Imagen Original e Imagen Invertida')
+    subplot(4,3,4)
+    imshow(yBinaryInvert)
+    title('Imagen sin Huecos con Color Invertido')
+    subplot(4,3,6)
+    imshow(yBinaryPro)
+    title('Imagen sin Huecos')
+
+%Plot the images with some operation
+subplot(4,3,7)
+imshow(and_img)
+title('AND Imagen sin Huecos con Color Invertido')
+    subplot(4,3,8)
+    imshow(or_img)
+    title('OR')
+        subplot(4,3,9)
+        imshow(xor_img)
+        title('XOR')
+subplot(4,3,10)
+imshow(and_img2)
+title('AND Imagen sin Huecos')
+    subplot(4,3,11)
+    imshow(or_img2)
+    title('OR')
+        subplot(4,3,12)
+        imshow(xor_img2)
+        title('XOR')
